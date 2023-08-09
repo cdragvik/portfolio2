8d01505e21709457f0360ca2a51f72598c87cb37
Cdragvik <cndragvik@gmail.com>
2023-08-09 12:58:52 +0200

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,7 @@
 
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom/client';
 import App from './App';
-
-import React from 'react'
+import React from 'react';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
